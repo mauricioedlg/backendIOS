888ee1219cf439dd0036d141d1311c9f2d326201
mauricioedlg <al02976904@tecmilenio.mx>
2025-10-17 16:53:01 -0600

    endpoints actualizados

--- backend/urls.py
+++ backend/urls.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from core.views import UsuarioViewSet, AccionViewSet, PortafolioViewSet, TransaccionViewSet
+from django.shortcuts import redirect
 
+# Registrar los endpoints en el router
 router = DefaultRouter()
 router.register(r'usuarios', UsuarioViewSet)
 router.register(r'acciones', AccionViewSet)
 router.register(r'portafolios', PortafolioViewSet)
 router.register(r'transacciones', TransaccionViewSet)
 
+# Rutas principales
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('', lambda request: redirect('/api/')),     # Redirige la raíz a /api/
+    path('admin/', admin.site.urls),                 # Panel de administración
+    path('api/', include(router.urls)),              # Incluye el router con tus endpoints
 ]
